dad2d75ead1be8e5661e193bd84ae4cf576cde70
Radmir <radmirka11@gmail.com>
2022-09-19 13:13:43 +0300

    reformat code

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -4,9 +4,10 @@ public class ProductManager {
     public ProductManager(ProductRepository repository) {
         this.repository = repository;
     }
+
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
-        for (Product product: repository.findAll()) {
+        for (Product product : repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
                 for (int i = 0; i <= result.length; i++) {
